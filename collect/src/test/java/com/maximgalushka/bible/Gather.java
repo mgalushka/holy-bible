de8ae199c74fa31dfc37f3266d7602c352b104cc
Maxim Galushka <mgalushka@gmail.com>
2014-04-18 15:27:42 +0300

    random method for visualization

--- collect/src/test/java/com/maximgalushka/bible/Gather.java
+++ collect/src/test/java/com/maximgalushka/bible/Gather.java
@@ -1,10 +1,10 @@
 package com.maximgalushka.bible;
 
 import com.mashape.unirest.http.JsonNode;
-
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 import java.util.Arrays;
+import java.util.Random;
 
 import static com.maximgalushka.bible.Constants.Command.*;
 
@@ -17,29 +17,54 @@ import static com.maximgalushka.bible.Constants.Command.*;
 public class Gather {
 
     private static String WRONG = "Please try again.";
+    private static String SPLIT = "\\s?\\d+\\.\\s?";
+    private static String SPLIT_VERSE = "\\s?\\d+\\s?";
 
-    public static void main(String[] args) throws UnirestException {
-        BibleApi api = new BibleApi();
+    private BibleApi api;
+    private String[] oldTestament;
+    private String[] newTestament;
+
+    private Random r;
+
+    public Gather() throws UnirestException {
+        this.api = new BibleApi();
+        this.r = new Random(System.currentTimeMillis());
+        init();
+    }
+
+    private void init() throws UnirestException {
         JsonNode nd = api.request(GET_BOOKS);
-        String[] newTestament = nd.getObject().get("The_New_Testament").toString().split("\\s?\\d+\\.\\s?");
-        String[] oldTestament = nd.getObject().get("The_Old_Testament").toString().split("\\s?\\d+\\.\\s?");
+        newTestament = nd.getObject().get("The_New_Testament").toString().split(SPLIT);
+        oldTestament = nd.getObject().get("The_Old_Testament").toString().split(SPLIT);
         System.out.println(Arrays.toString(newTestament));
         System.out.println(Arrays.toString(oldTestament));
+    }
 
-        for (String book : newTestament) {
-            if ("".equals(book)) continue;
-            boolean incorrect = false;
-            int id = 1;
-            while (!incorrect) {
-                JsonNode chapter = api.request(
-                        String.format("%s?Book=%s&chapter=%d", GET_CHAPTER, book, id++));
-                if (chapter.getObject().get("Output").toString().contains(WRONG)) {
-                    incorrect = true;
-                } else {
-                    System.out.printf("[%s]: %s \n", book, chapter);
-                }
+    public String random() throws UnirestException {
+        int t = r.nextInt(2);
+        String[] testament = (t == 0) ? oldTestament : newTestament;
+        String book = testament[r.nextInt(testament.length - 1) + 1];
+
+        int chapter = r.nextInt(200);
+        while (true) {
+            JsonNode chapters = api.request(
+                    String.format("%s?Book=%s&chapter=%d", GET_CHAPTER, book, chapter));
+            String ch = chapters.getObject().get("Output").toString();
+            if (!ch.contains(WRONG)) {
+                String[] tokens = ch.split(SPLIT_VERSE);
+                return tokens[r.nextInt(tokens.length)];
+            } else {
+                chapter /= 2;
             }
         }
+    }
+
+    public static void main(String[] args) throws UnirestException {
+        Gather g = new Gather();
+        System.out.println(g.random());
+        System.out.println(g.random());
+        System.out.println(g.random());
+        System.out.println(g.random());
     }
 
 }
